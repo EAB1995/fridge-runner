fa20e3f3885b5c9172a73f26f68230d4943dc8c4
EAB <etienneavissebrown@gmail.com>
2024-10-09 21:03:35 +0100

    frontend build working in isolation

--- boot-env.sh
+++ boot-env.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-echo "Building docker image for frontend..."
-cd /c/projects/fridge-runner/fridge-runner/frontend
-docker build -t fridge-runner-frontend
-
-echo "Starting image..."
-docker run -d -p 3000:80 fridge-runner-frontend
-
 echo "Starting backend image..."
 docker run -d -p 8000:8000 amazon/dynamodb-local
 
 echo "Starting python env..."
 source /c/projects/fridge-runner/fridge-runner/backend/service-layer-python/Scripts/activate
 
-echo "starting SAM..."
+echo "Starting SAM..."
 cd /c/projects/fridge-runner/fridge-runner/backend/lambda-service/fridge-runner
-sam.cmd local start-api --docker-network host
+sam.cmd local start-api --docker-network host -p 3001 &
+
+echo "Building docker image for frontend..."
+cd /c/projects/fridge-runner/fridge-runner/frontend
+docker-compose build --no-cache
+docker-compose up -d
 
 echo "Goodbye."
