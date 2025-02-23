193c75acc8cd1bc1493a34145443d51e2460d50b
EAB <etienneavissebrown@gmail.com>
2025-02-23 13:48:44 +0000

    Squashed commit of the following:
    commit 5ac82c7db75f6511762d8515a75c6859affddc80
    Author: EAB <etienneavissebrown@gmail.com>
    Date:   Sun Feb 23 13:43:43 2025 +0000
    
        startup script cleanup
    
    commit 5b800afcbb29fde14797e0a7a0b2530ba8e91e86
    Author: EAB <etienneavissebrown@gmail.com>
    Date:   Sun Feb 23 13:43:16 2025 +0000
    
        startup script cleanup

--- boot-env-local.sh
+++ boot-env-local.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-#TODO###############
-# We are trying to set up the python virtual env in /lambda-service/fridgerunnner, 
-# as before the env was loaded in to /service-layer-python, which was the old Flask directory (now deleted).
-# This is to ifx an issue with check_connection_dynamodb.py, which is unable to import boto3, which I suspect is 
-# because of the duelling virtual envs. 
-
-# We are trying to audit the dynamo db connection outside of the AWS CLI context to see if it can be reached
-# and troubleshoot the networking, as save_input.py is the first lambda to try and actually reach the db, 
-# whereas hello world is only reaching the lambda layer, and not trying to actually post to dynamo. 
-
 printf "\nStarting backend image...\n"
 docker run -d -p 8000:8000 amazon/dynamodb-local
 
 printf "\nStarting python env...\n"
-#Not sure if this line is redundant now that the python env seems activated by default (Ctrl + Shft + P). 
-#Also not sure if this is activating properly as part of this startup script
 source /c/projects/fridge-runner/fridge-runner/backend/lambda-service/fridge-runner/venv/Scripts/activate
 
 printf "\nStarting SAM locally...\n"
@@ -34,6 +22,7 @@ printf "\nBuilding test tables...\n"
 cd /c/projects/fridge-runner/fridge-runner/fridge-runner/backend
 ./test-table-create.sh
 
+#Not being called currently
 printf "\nGetting test tables...\n"
 cd /c/projects/fridge-runner/fridge-runner/fridge-runner/backend
 ./test-table-get.sh
